b49026a7a58d0295d9334ff2f4ae08e7e5e8b8aa
hoax <kvnrivers@gmail.com>
2023-12-11 12:15:42 +0530

    canvas animation w minimap

--- app/src/providers/service/aws/services/dynamodb.go
+++ app/src/providers/service/aws/services/dynamodb.go
@@ -29,9 +29,42 @@ func (s Services) GetDynamodbTables(respch chan<- types.GetDataOutput, wg *sync.
 		}
 		return
 	}
+	swg := &sync.WaitGroup{}
+	ch := make(chan *dynamodb.DescribeTableOutput, len(result.TableNames))
+	for _, tableName := range result.TableNames {
+		swg.Add(1)
+		go s.describeTable(ch, swg, &tableName)
+	}
+	go func() {
+		swg.Wait()
+		close(ch)
+	}()
+
+	res := []*dynamodb.DescribeTableOutput{}
+	for data := range ch {
+		if data != nil {
+			res = append(res, data)
+		}
+	}
 	respch <- types.GetDataOutput{
 		Name:   constants.DYNAMODB,
-		Result: result,
+		Result: res,
 	}
+	return
+}
+
+func (s Services) describeTable(respch chan<- *dynamodb.DescribeTableOutput, wg *sync.WaitGroup, tableName *string) {
+	defer wg.Done()
+	logger.Logger.Log("fetching table details for tableName:", tableName)
+	instance := dynamodb.NewFromConfig(s.Config)
+	result, err := instance.DescribeTable(context.TODO(), &dynamodb.DescribeTableInput{
+		TableName: tableName,
+	})
+	if err != nil {
+		logger.Logger.Error("Failed to describe table...", err)
+		respch <- nil
+		return
+	}
+	respch <- result
 	return
 }
