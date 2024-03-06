4d40204bffa5e5af9f37a3c6a1d6a30fe7f7d991
hoax <kvnrivers@gmail.com>
2024-03-06 13:53:39 +0530

    optimized landing page
    Added purge css, fixed layout shift issue

--- landing-page/www/astro.config.mjs
+++ landing-page/www/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
+import compressor from "astro-compressor";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +11,7 @@ export default defineConfig({
 		priority: .8,
 		changefreq: "weekly",
 		lastmod: new Date("2023-02-01")
-	}) ],
+	}), compressor(), purgecss() ],
 	site: "https://getcloudfriendly.com",
 	trailingSlash: "ignore",
 	// required for cloudflare to remove trailing slash ('/')
@@ -18,6 +20,6 @@ export default defineConfig({
 	adapter: cloudflare(),
 	image: {
 		domains: [ "astro.build" ],
-		service: { entrypoint: "astro/assets" },	
+		service: { entrypoint: "astro/assets" }
 	}
 });
--- landing-page/www/tailwind.config.cjs
+++ landing-page/www/tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [ "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}" ],
@@ -7,6 +9,8 @@ module.exports = {
 				"site-gradient":
                     "linear-gradient(81.02deg, #4d91ff -23.47%, #b14bf4 45.52%, #fa5560 114.8%)",
 			},
+			fontFamily: { jakarta: [ "Plus Jakarta Sans", ...defaultTheme.fontFamily.sans ] },
+			height: { "500": "500px" }
 		},
 	},
 	plugins: [],
